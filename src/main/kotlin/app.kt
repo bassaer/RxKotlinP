9e8595abf2bdf357b96c9b5ea69f55a96d047dfb
Tsubasa Nakayama <app.nakayama@gmail.com>
2018-04-21 19:23:53 +0900

    add flowable sample

--- src/main/kotlin/app.kt
+++ src/main/kotlin/app.kt
@@ -1,14 +1,27 @@
 import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.rxkotlin.toFlowable
 import io.reactivex.rxkotlin.toObservable
 
 fun main(args: Array<String>) {
-    val list = listOf("alpha", "beta", "gamma", "delta", "epsilon")
+    val data1 = listOf("alpha", "beta", "gamma", "delta", "epsilon")
 
-    list.toObservable()
+    data1.toObservable()
             .filter { it.length >= 5 }
             .subscribeBy(
                     onNext = { println(it)},
                     onError = { it.printStackTrace() },
                     onComplete = { println("Done.")}
             )
+    val data2 = (1..10).toList()
+    val flowable = data2.toFlowable()
+            .filter{it % 2 == 0}
+            .map { it * 100 }
+    flowable.subscribe{
+        println(it)
+    }
+
+
+    flowable.subscribe{
+        println(it)
+    }
 }
